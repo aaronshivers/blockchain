aca92984a0ddcac37dd005681c8cb3f80437bc5b
aaronshivers <aaron@moonmail.org>
2019-06-07 09:23:00 -0500

    making progress

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
 const crypto = require('crypto')
 
-const chain = []
-
-const genesisBlock = {
-  index: 0,
-  timestamp: new Date().getTime(),
-  data: 'Genesis Data',
-  previousHash: '-1'
+const user = {
+  name: 'Bob',
+  chain: [],
+  currentBlock: {}
 }
 
 const createHash = ({ timestamp, data, index, previousHash}) => {
@@ -14,12 +11,40 @@ const createHash = ({ timestamp, data, index, previousHash}) => {
   return crypto.createHash('SHA256').update(index + timestamp + data + previousHash).digest('hex')
 }
 
-const addToChain = block => {
+const initializeIt = user => {
 
-  
+  const genesisBlock = {
+    index: 0,
+    timestamp: new Date().getTime(),
+    data: 'Genesis Data',
+    previousHash: '-1'
+  }
+
+  const hash = createHash(genesisBlock)
+  const updatedChain = user.chain.push(hash)
+  Object.assign(user, { currentBlock: genesisBlock })
+  return Object.assign({}, user, { chain: updatedChain })
 }
 
-chain.push(createHash(genesisBlock))
+const addToChain = (user, data) => {
+// console.log(user)
+  const newBlock = {
+    // index: user.previousBlock[0].index++,
+    timestamp: 1234,
+    // timestamp: new Date().getTime(),
+    data,
+    previousHash: user.chain[user.chain.length - 1]
+  }
 
+  const hash = createHash(newBlock)
+  const previousBlock = newBlock
+  // console.log(previousBlock)
+  const updatedChain = user.chain.push(hash)
+  return Object.assign({}, user, { previousBlock: newBlock, chain: updatedChain })
+}
 
-console.log(chain)
+initializeIt(user)
+// addToChain(user, 'data')
+// addToChain(user, 'more data')
+
+console.log(user)
