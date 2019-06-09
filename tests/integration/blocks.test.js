3956da5e666d4de7bfbcf9667651188b7aef8450
aaronshivers <aaron@moonmail.org>
2019-06-09 01:11:47 -0500

    added route for GET /api/blocks

---
+++ tests/integration/blocks.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest')
+const app = require('../../app')
+
+const Blockchain = require('../../blockchain')
+
+describe('/api/blocks', () => {
+
+  // GET /api/blocks
+  describe('GET /api/blocks', () => {
+
+    // it('should respond 200', async () => {
+
+    //   const blockhain = new Blockchain()
+
+    //   await request(app)
+    //     .get('/')
+    //     .send(blockchain)
+    //     .expect(200)
+    //     .expect(res => {
+    //       expect(res.json).toContain(blockchain.chain)
+    //     })
+    // })
+  })
+})
