27597df621393f2ab5eca6018c009725898556f8
aaronshivers <aaron@moonmail.org>
2019-06-08 19:59:42 -0500

    implemented adjustDifficulty()

--- block.js
+++ block.js
@@ -18,18 +18,19 @@ class Block {
 
   static mineBlock({ lastBlock, data }) {
 
-    let hash, timestamp
     const lastHash = lastBlock.hash
-    const { difficulty } = lastBlock
+    let hash, timestamp
+    let { difficulty } = lastBlock
     let nonce = 0
 
     do {
 
       nonce++
       timestamp = Date.now()
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
       hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
 
-    } while(hash.substring(0, difficulty) !== '0'.repeat(difficulty))
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
     return new this({ timestamp, lastHash, data, nonce, difficulty, hash })
   }
@@ -37,6 +38,8 @@ class Block {
   static adjustDifficulty({ originalBlock, timestamp }) {
 
     const { difficulty } = originalBlock
+
+    if (difficulty < 1) return 1
 
     const difference = timestamp - originalBlock.timestamp
 
