45b03c43ce671abaa988a487c2456c0b508fc668
aaronshivers <aaron@moonmail.org>
2019-06-09 22:08:35 -0500

    setup transactions

---
+++ wallet/transaction.js
@@ -0,0 +1,19 @@
+const uuidv1 = require('uuid/v1')
+
+class Transaction {
+  constructor({ senderWallet, recipient, amount }) {
+    this.id = uuidv1
+    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount})
+  }
+
+  createOutputMap({ senderWallet, recipient, amount}) {
+    const outputMap = {}
+
+    outputMap[recipient] = amount
+    outputMap[senderWallet.publicKey] = senderWallet.balance - amount
+
+    return outputMap
+  }
+}
+
+module.exports = Transaction
