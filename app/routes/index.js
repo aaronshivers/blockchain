f8b9a3ef1d2dda2ac629d5702ad406a2e8a9eeac
aaronshivers <aaron@moonmail.org>
2019-06-11 11:18:18 -0500

    setup GET /api/mine-transactions

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,11 +6,15 @@ const Blockchain = require('../../block/index')
 const PubSub = require('../pubsub')
 const TransactionPool = require('../../wallet/transaction-pool')
 const Wallet = require('../../wallet')
+const TransactionMiner = require('../transaction-miner')
 
 const blockchain = new Blockchain()
 const transactionPool = new TransactionPool()
 const wallet = new Wallet
 const pubsub = new PubSub({ blockchain, transactionPool, wallet })
+const transactionMiner = new TransactionMiner({
+  blockchain, transactionPool, wallet, pubsub
+})
 
 const ROOT_NODE_ADDRESS = `http://localhost:${ process.env.PORT }`
 
@@ -74,6 +78,22 @@ router.get('/api/transaction-pool-map', (req, res) => {
 
   } catch (error) {
 
+    res.json({ error: error.message })
+
+  }
+})
+
+// GET /api/mine-transactions
+router.get('/api/mine-transactions', (req, res) => {
+
+  try {
+
+    transactionMiner.mineTransactions()
+
+    res.redirect('/api/blocks')
+    
+  } catch (error) {
+    
     res.json({ error: error.message })
 
   }
