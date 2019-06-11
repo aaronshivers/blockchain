ba943da935a156e532bdc141c688be5d40982f47
aaronshivers <aaron@moonmail.org>
2019-06-11 09:59:56 -0500

    added ability to clear blockchain transactions

--- app/pubsub.js
+++ app/pubsub.js
@@ -51,13 +51,15 @@ class PubSub {
   }
 
   subscribeToChannels() {
-    this.pubnub.subscribe({
-      channels: [ Object.values(CHANNELS) ]
-    })
+   this.pubnub.subscribe({
+     channels: [ Object.values(CHANNELS) ]
+   })
   }
 
   publish({ channel, message }) {
-    this.pubnub.publish({ channel, message })
+    this.pubnub.unsubscribe({ channel })
+    setTimeout(() => this.pubnub.publish({ channel, message }), 3000)
+    setTimeout(() => this.pubnub.subscribe({ channels: [ Object.values(CHANNELS) ] }), 6000)
   }
 
   broadcastChain() {
