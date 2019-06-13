c3fa6dc7551a2fe97c5abcc35362f5700074604d
aaronshivers <aaron@moonmail.org>
2019-06-13 07:27:25 -0500

    setup App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './components/App'
 
 render(
-  <div>Cryptochain in React! Second Live Update!</div>,
+  <App />,
   document.getElementById('root')
 )
